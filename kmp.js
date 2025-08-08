e46882e9ac867ee714b2965ece53d49b9214b26e
ass-kofu <132200115+ass-kofu@users.noreply.github.com>
2025-08-08 09:37:33 +0300

    Add files via upload

--- kmp.js
+++ kmp.js
@@ -14,13 +14,38 @@ productTab.forEach((item, i) => {
   let productContainerWidth = productContainer.width;
 })
 next.onclick = function(){
-  itemActive = itemActive + 1;
-  if (itemActive => countItem){
-    itemActive = 0;
+ showSlider('next')
+}
+prev.onclick = function(){
+  showSlider('prev')
+}
+function showSlider(type){
+  if (type === "next"){
+    item.appendChild(item[0])
+    item.classList.add('next')
+  } else {
+    list.prev(item[item.length - 1])
+    item.classList.add('prev')
   }
-  showSlider();
-};
+}
+
 function showSlider(){
   let itemACtiveOld =  items['.active'];
   itemACtiveOld.classList.remove('active');
+}
+function showSidebar(){
+  let sideBar = document.querySelector('.header .header-2 .logo .menu-list');
+  sideBar.style.visibility = 'visible'
+}
+function hideSidebar(){
+  let hideSidebar = document.querySelector('.header .header-2 .logo .menu-list');
+  hideSidebar.style.visibility = 'hidden';
+}
+function showList(){
+  let showList = document.querySelector('.header .header-2 .search ul');
+  showList.style.visibility = 'visible';
+}
+function body(){
+  let hideList = document.querySelector('.header .header-2 .search ul');
+  hideList.style.visibility = 'hidden';
 }
